54a208176d358171b08299f1aea11bee20004b3c
Alfred Robert Rowe <alfred@ncodedevlabs.com>
2012-07-10 15:04:52 +0000

    Few changes for VERSION 0.1.0 Release

--- README.md
+++ README.md
@@ -18,7 +18,32 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+Setup your SMSGH API username and password
+
+	SmsghSms.api_username = "YOUR_USERNAME"
+	SmsghSms.api_password = "YOUR_PASSWORD"
+	
+Optionally set global Sender ID
+
+	SmsghSms.api_senderid = "YOUR_SENDERID"
+	
+Sending an SMS Message
+
+	SmsghSms.push(:to => 'PHONE-NUMBER', :msg => "Your SMS Message goes here")
+	
+How to override the global SENDER ID config
+
+	SmsghSms.push(:to => 'PHONE-NUMBER', :msg => "Your SMS Message goes here", :from => "SENDERID")
+	
+Sample Results Returned
+
+	{:status=>200, :notice=>"OK: 62357215"}
+	
+Any returned status code other than 200 is a failed response.
+
+## About SMSGH
+
+SMSGH is the premier messaging company in Ghana. Find out more about them at http://smsgh.com
 
 ## Contributing
 
--- lib/smsgh_sms.rb
+++ lib/smsgh_sms.rb
@@ -5,17 +5,21 @@ module SmsghSms
   API_URL = "http://api.smsgh.com/v2/messages/send?"
   @@api_username = nil
   @@api_password = nil
-  @@api_senderid = "SMSGH-API"
+  @@api_senderid = "SMSGHAPI"
   
   # Expects :msg, :to and an optional :from param
   # The :from param defaults to @@api_senderid when its omitted
-  
   def self.push(options={})
-    api_base = "#{API_URL}&username=#{@@api_username}&password=#{@@api_password}&from=#{@@api_senderid}"
+    
+    sender_id = options[:from].nil? ? @@api_senderid : options[:from]
+    api_base = "#{API_URL}&username=#{@@api_username}&password=#{@@api_password}&from=#{sender_id}"
+    
     url = "#{api_base}&text=#{CGI.escape(options[:msg])}&to=#{options[:to]}"
     raise ArgumentError, ':msg and :to params expected' if options[:msg].nil? || options[:to].nil?
+    
     response = CurbFu.get(url)
-    {:status => response.status, :message => response.body}
+    {:status => response.status, :notice => response.body}
+    
   end
   
   def self.api_url(url=''); @@api_base + url; end
@@ -25,4 +29,5 @@ module SmsghSms
   def self.api_password; @@api_password; end
   def self.api_senderid=(api_senderid); @@api_senderid = api_senderid; end
   def self.api_senderid; @@api_senderid; end
+  
 end
