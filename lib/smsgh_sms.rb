09cbcb76e14efa27366a8186695cdd9db081fa8f
Alfred Robert Rowe <alfred@ncodedevlabs.com>
2012-07-10 14:15:19 +0000

    Fixed few things

--- lib/smsgh_sms.rb
+++ lib/smsgh_sms.rb
@@ -10,11 +10,19 @@ module SmsghSms
   # Expects :msg, :to and an optional :from param
   # The :from param defaults to @@api_senderid when its omitted
   
-  def self.pushsms(options={})
-    url_construct = "#{API_URL}&username=#{@@api_username}&password=#{@@api_password}&from=#{@@api_senderid}"
-    full_url = "#{url_construct}&text=#{CGI.escape(options[:msg])}&to=#{options[:to]}"
+  def self.push(options={})
+    api_base = "#{API_URL}&username=#{@@api_username}&password=#{@@api_password}&from=#{@@api_senderid}"
+    url = "#{api_base}&text=#{CGI.escape(options[:msg])}&to=#{options[:to]}"
     raise ArgumentError, ':msg and :to params expected' if options[:msg].nil? || options[:to].nil?
-    response = CurbFu.get(full_url)
+    response = CurbFu.get(url)
     {:status => response.status, :message => response.body}
   end
+  
+  def self.api_url(url=''); @@api_base + url; end
+  def self.api_username=(api_username); @@api_username = api_username; end
+  def self.api_username; @@api_username; end
+  def self.api_password=(api_password); @@api_password = api_password; end
+  def self.api_password; @@api_password; end
+  def self.api_senderid=(api_senderid); @@api_senderid = api_senderid; end
+  def self.api_senderid; @@api_senderid; end
 end
